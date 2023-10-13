f1b048b866830b38a75b8a6e2e1527def1c6ba14
antonioqueb <cardstillo@gmail.com>
2023-10-13 12:37:45 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'tobaccometricspro',
+    'version': '1.0.0.0',  # Aquí puedes aumentar la versión
+    'category': 'Administration',
+    'summary': 'Herramientas de análisis y visualización para la industria tabacalera.',
+    'author': 'Antonio Queb',
+    'website': 'https://www.fingrow.co',
+    'depends': ['base', 'web'],
+    'assets': {
+        'web.assets_backend': [
+            'kpi_dashboard_fingrow/static/src/js/*.js',
+        ]
+    },
+    'data': [
+        'views/menu.xml',  # Añadiendo la nueva vista XML
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ static/src/js/header.js
@@ -0,0 +1,22 @@
+/** @odoo-module **/
+
+import { Component, tags } from "@odoo/owl";
+
+class Header extends Component {
+    static template = tags.xml/*xml*/ `
+        <header>
+            <h1>Tobacco Metrics Pro</h1>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+            </nav>
+        </header>
+    `;
+}
+
+//Register the component to be used in the template
+
+odoo.define('tobaccometricspro.header', function (require) {
+    return Header;
+
+});
