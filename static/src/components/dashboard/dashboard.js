56fd4f979d2ff60a7d9bfd74dedf41abbbe135ca
antonioqueb <cardstillo@gmail.com>
2023-10-14 06:32:15 -0600

    Up

--- static/src/components/dashboard/dashboard.js
+++ static/src/components/dashboard/dashboard.js
@@ -4,8 +4,14 @@ import { registry } from "@web/core/registry";
 const { Component, tags } = owl;
 
 export class Dashboard extends Component {
+    constructor() {
+        super(...arguments);
+        console.log("Dashboard component created");
+    }
 }
 
 Dashboard.template = "TobaccoMetricsPro.dashboard";
 
+// Log to console when adding to the registry to confirm it's being executed
+console.log("Adding Dashboard to the registry");
 registry.category("addons").add("TobaccoMetricsPro", Dashboard);
