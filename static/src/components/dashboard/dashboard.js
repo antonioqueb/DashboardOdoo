56ae032a290bbf2f28d040f9e2b82759114846a3
antonioqueb <cardstillo@gmail.com>
2023-10-14 14:26:03 -0600

    Up

--- static/src/components/dashboard/dashboard.js
+++ static/src/components/dashboard/dashboard.js
@@ -8,10 +8,39 @@ const { Component, tags, onWillStart, useRef, onMounted } = owl;
 
 export class Dashboard extends Component {
     setup () {
-
+        this.chartRef = useRef("chart");
         onWillStart(async () => {
             await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js");
         });
+
+        onMounted(()=>{
+            const data = [
+                { year: 2010, count: 10 },
+                { year: 2011, count: 20 },
+                { year: 2012, count: 15 },
+                { year: 2013, count: 25 },
+                { year: 2014, count: 22 },
+                { year: 2015, count: 30 },
+                { year: 2016, count: 28 },
+              ];
+            
+              new Chart(
+                document.getElementById('acquisitions'),
+                {
+                  type: 'bar',
+                  data: {
+                    labels: data.map(row => row.year),
+                    datasets: [
+                      {
+                        label: 'Acquisitions by year',
+                        data: data.map(row => row.count)
+                      }
+                    ]
+                  }
+                }
+              );
+
+        })
 
     }
 }
