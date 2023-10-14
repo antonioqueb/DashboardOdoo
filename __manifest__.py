f1416a38ac2500991b39e14c3afebdb906bf2173
antonioqueb <cardstillo@gmail.com>
2023-10-13 21:30:30 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -1,19 +1,22 @@
 {
     'name': 'TobaccoMetricsPro',
-    'version': '1.0.1', 
+    'version': '1.0.2', 
     'category': 'Administration',
     'summary': 'Herramientas de análisis y visualización para la industria tabacalera.',
     'author': 'Antonio Queb',
     'website': 'https://www.fingrow.co',
-    'depends': ['base', 'web'],
+    'depends': ['base', 'web', ],
     'assets': {
         'web.assets_backend': [
-            'TobaccoMetricsPro/static/src/js/*.js',
+            'TobaccoMetricsPro/static/src/js/header.js',
         ]
     },
     'data': [
         'views/menu.xml'
     ],
+    
     'installable': True,
     'application': True,
+    'auto_install': False,
+
 }
