a4d8135d7c7cb41b77c01334bb311f795a9345d4
antonioqueb <cardstillo@gmail.com>
2023-10-14 11:06:45 -0600

    Up

--- static/src/components/metric_card/metric_card.js
+++ static/src/components/metric_card/metric_card.js
@@ -2,7 +2,11 @@
 
 const { Component } = owl;
 
-export class MetricCard extends Component {}
+export class MetricCard extends Component {
+    formatNumber(num) {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+    }
+}
 
 
 MetricCard.template = "TobaccoMetricsPro.MetricCard";
