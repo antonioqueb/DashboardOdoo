be4ea0eca3b633b057a1b20ffabd7165fe61feed
antonioqueb <cardstillo@gmail.com>
2023-10-14 11:55:57 -0600

    Up

--- static/src/components/metric_card/metric_card.js
+++ static/src/components/metric_card/metric_card.js
@@ -8,30 +8,32 @@ export class MetricCard extends Component {
     }
 
     formatNumber(num, name) {
-        console.log("Nombre:", name);
-        console.log("Número:", num);
-    
-        if (!num) {
-            return '0';
-        }
-    
-        if (typeof num === 'string') {
-            num = parseFloat(num); // Cambié esto a parseFloat para manejar números decimales
-        }
-    
+        if (!num) return '0';
+
+        // Convertir la entrada a un número si es una cadena
+        if (typeof num === 'string') num = parseFloat(num);
+
+        let formattedNumber;
         switch (name.trim()) {
             case 'Calidad':
-                console.log("Es Calidad");
-                return num.toFixed(1).toLocaleString() + '%'; // Corregido para formatear números con comas
+                formattedNumber = num.toFixed(1) + '%';
+                break;
             case 'Ventas':
             case 'Costos':
             case 'Utilidad':
-                console.log(name.trim() + " en dólares");
-                return '$' + num.toFixed().toLocaleString('es-MX'); // Corregido para formatear números con comas
+                formattedNumber = '$' + num.toFixed(0).toLocaleString('es-MX');
+                break;
             default:
-                console.log("Caso no manejado para", name.trim());
-                return num.toLocaleString(); // Usando toLocaleString para formatear números
+                formattedNumber = num.toLocaleString();
+                break;
         }
+
+        // Logging para depuración - Puedes eliminarlo en producción
+        console.log('Nombre:', name);
+        console.log('Número:', num);
+        console.log('Formateado:', formattedNumber);
+
+        return formattedNumber;
     }
 }
 
