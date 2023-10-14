fd0e9b01dd23ea9210690529c0395b6b5b225d9a
antonioqueb <cardstillo@gmail.com>
2023-10-14 17:51:41 -0600

    Up

--- static/src/components/chart_renderer/chart_renderer.js
+++ static/src/components/chart_renderer/chart_renderer.js
@@ -77,4 +77,109 @@ export class ChartRenderer extends Component {
     }
 }
 
+const myData = {
+    labels: ['Mayo', 'Junio', 'Julio'],
+    datasets: [
+        {
+            label: 'Ventas netas',
+            data: [139, 135, 139],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Costo neto',
+            data: [52, 54, 52],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Utilidad Bruta',
+            data: [87, 80, 87],
+            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+            borderColor: 'rgba(255, 206, 86, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Gasto de admón.',
+            data: [7, 14, 6],
+            backgroundColor: 'rgba(153, 102, 255, 0.2)',
+            borderColor: 'rgba(153, 102, 255, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Gasto de Venta',
+            data: [25, 23, 30],
+            backgroundColor: 'rgba(255, 159, 64, 0.2)',
+            borderColor: 'rgba(255, 159, 64, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Gastos Totales',
+            data: [32, 37, 37],
+            backgroundColor: 'rgba(201, 203, 207, 0.2)',
+            borderColor: 'rgba(201, 203, 207, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Impuestos Federales',
+            data: [8, 7, 7],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Utilidad Neta',
+            data: [48, 37, 43],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }
+    ]
+};
+
+const myDataProductos = {
+    labels: ['Mayo', 'Junio', 'Julio'],
+    datasets: [
+        {
+            label: 'Económicos',
+            data: [486, 430, 397],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Laredo',
+            data: [103, 117, 103],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Península',
+            data: [30, 13, 26],
+            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+            borderColor: 'rgba(255, 206, 86, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Studio 54',
+            data: [97, 88, 176],
+            backgroundColor: 'rgba(153, 102, 255, 0.2)',
+            borderColor: 'rgba(153, 102, 255, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Senator',
+            data: [42, 26, 24],
+            backgroundColor: 'rgba(255, 159, 64, 0.2)',
+            borderColor: 'rgba(255, 159, 64, 1)',
+            borderWidth: 1
+        },
+        // Puedes añadir más productos aquí...
+    ]
+};
+
+
 ChartRenderer.template = "TobaccoMetricsPro.ChartRenderer";
--- static/src/components/dashboard/dashboard.js
+++ static/src/components/dashboard/dashboard.js
@@ -2,109 +2,52 @@
 
 import { registry } from "@web/core/registry";
 import { MetricCard } from "../metric_card/metric_card.js";
-import { ChartRenderer } from "../chart_renderer/chart_renderer.js";
 import { loadJS } from "@web/core/assets";
 
-const { Component, tags, onWillStart, useRef, onMounted } = owl;
+const { Component, hooks } = owl;
+const { useRef, onWillStart, onMounted } = hooks;
 
 export class Dashboard extends Component {
-    setup () {
+    constructor() {
+        super(...arguments);
         this.chartRef = useRef("chart");
-        onWillStart(async () => {
-            await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js");
-        });
+        this.onWillStart = onWillStart(this.willStart.bind(this));
+        this.onMounted = onMounted(this.mounted.bind(this));
+    }
 
-        onMounted(()=>{
-            const data = {
-                labels: ['Mayo', 'Junio', 'Julio'],
-                datasets: [
-                    {
-                        label: 'Ventas netas',
-                        data: [139, 135, 139],
-                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                        borderColor: 'rgba(255, 99, 132, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Costo neto',
-                        data: [52, 54, 52],
-                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Utilidad Bruta',
-                        data: [87, 80, 87],
-                        backgroundColor: 'rgba(255, 206, 86, 0.2)',
-                        borderColor: 'rgba(255, 206, 86, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Gasto de admón.',
-                        data: [7, 14, 6],
-                        backgroundColor: 'rgba(153, 102, 255, 0.2)',
-                        borderColor: 'rgba(153, 102, 255, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Gasto de Venta',
-                        data: [25, 23, 30],
-                        backgroundColor: 'rgba(255, 159, 64, 0.2)',
-                        borderColor: 'rgba(255, 159, 64, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Gastos Totales',
-                        data: [32, 37, 37],
-                        backgroundColor: 'rgba(201, 203, 207, 0.2)',
-                        borderColor: 'rgba(201, 203, 207, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Impuestos Federales',
-                        data: [8, 7, 7],
-                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                        borderColor: 'rgba(54, 162, 235, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Utilidad Neta',
-                        data: [48, 37, 43],
-                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                        borderColor: 'rgba(255, 99, 132, 1)',
-                        borderWidth: 1
-                    }
-                ],
-            };
+    async willStart() {
+        await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js");
+    }
 
-            if (this.chartRef.el) {
-                new Chart(this.chartRef.el.getContext('2d'), {
-                    type: 'bar',
-                    data: data,
-                    options: {
-                        responsive: true,
-                        plugins: {
-                            title: {
-                                display: true,
-                                text: 'Valores en Mn Mxn (millones)',
-                                fontSize: 16
-                            },
-                        },
-                        scales: {
-                            x: {
-                                stacked: true,
-                            },
-                            y: {
-                                stacked: true,
-                                beginAtZero: true,
-                            },
+    mounted() {
+        if (this.chartRef.el && this.props.data) {
+            new Chart(this.chartRef.el.getContext('2d'), {
+                type: this.props.type || 'bar', // Acepta el tipo de gráfico como una propiedad, 'bar' como default
+                data: this.props.data,
+                options: {
+                    responsive: true,
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: this.props.title || 'Default Title', // Acepta el título como una propiedad
+                            fontSize: 16
                         },
                     },
-                });
-            }
-        });
+                    scales: {
+                        x: {
+                            stacked: true,
+                        },
+                        y: {
+                            stacked: true,
+                            beginAtZero: true,
+                        },
+                    },
+                },
+            });
+        }
     }
 }
+
 
 Dashboard.template = "TobaccoMetricsPro.Dashboard";
 Dashboard.components = { MetricCard, ChartRenderer};
