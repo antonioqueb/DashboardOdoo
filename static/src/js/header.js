42fcf9b5ea5d69e67b673f98bada686199efa538
antonioqueb <cardstillo@gmail.com>
2023-10-13 21:32:41 -0600

    Up

--- static/src/js/header.js
+++ static/src/js/header.js
@@ -2,8 +2,10 @@
 
 import { Component, tags } from "@odoo/owl";
 
+const { xml } = tags;
+
 class Header extends Component {
-    static template = tags.xml/*xml*/`
+    static template = xml/*xml*/ `
         <header>
             <h1>Tobacco Metrics Pro</h1>
             <nav>
