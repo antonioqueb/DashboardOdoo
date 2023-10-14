0a529e0929b055e3c3208972c400daf4e53693ce
antonioqueb <cardstillo@gmail.com>
2023-10-14 09:59:32 -0600

    Up

---
+++ app.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Directorio donde se encuentra tu repositorio Git
+REPO_DIR="$(dirname "$0")"
+
+while true; do
+    # Cambia al directorio del repositorio
+    cd "$REPO_DIR"
+
+    # Ejecuta git pull
+    git pull
+
+    # Espera 30 segundos antes de la próxima ejecución
+    sleep 30
+done
