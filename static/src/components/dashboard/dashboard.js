f4674b58d964a3510293eb59910c4344f7d2c101
antonioqueb <cardstillo@gmail.com>
2023-10-14 08:01:58 -0600

    Up

--- static/src/components/dashboard/dashboard.js
+++ static/src/components/dashboard/dashboard.js
@@ -16,4 +16,4 @@ console.log(Dashboard.template);
 // Log to console when adding to the registry to confirm it's being executed
 console.log("Adding Dashboard to the registry");
 
-registry.category("actions").add("Dashboard", Dashboard);
+registry.category("main_components").add("Dashboard", Dashboard);
