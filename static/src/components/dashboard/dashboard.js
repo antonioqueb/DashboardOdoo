53115ed56255b3653dfd723833ae1b08d4553e68
antonioqueb <cardstillo@gmail.com>
2023-10-14 21:12:53 -0600

    Up

--- static/src/components/dashboard/dashboard.js
+++ static/src/components/dashboard/dashboard.js
@@ -3,12 +3,12 @@
 import { registry } from "@web/core/registry";
 import { MetricCard } from "../metric_card/metric_card.js";
 import { ChartRenderer } from "../chart_renderer/chart_renderer.js";
-import { useService, useState } from "@web/core/utils/hooks"; // Asegúrate de que useState esté disponible para importar desde aquí
-import { Component, tags } from "@odoo/owl"; // Parece que olvidaste importar Component y tags, añádelo según sea necesario
+import { useService, useState } from "@web/core/utils/hooks"; // Verifica la importación de useState
+import { Component, tags } from "@odoo/owl"; // Verifica la importación de Component y tags
 
 export class Dashboard extends Component {
     setup() {
-        this.state = useState({
+        this.state = useState({  // Asegúrate de que useState está disponible y es una función
             ventas: {
                 title: "Ventas",
                 value: 0,
@@ -16,22 +16,21 @@ export class Dashboard extends Component {
             },
             period: 90,
         });
-        
+
         this.orm = useService("orm");
 
         this.constructor.onWillStart = async () => {
             await this.getQuotations();
         };
     }
+    
     async onChangePeriod(ev) {
         this.state.date = moment().subtract(this.state.period, "days").format("L");
-        await this.getQuotations()
+        await this.getQuotations();
     }
 
-
-
     async getQuotations() {
-        const data = await this.orm.searchCount("sale.order", [['state', 'in', ['sale', 'done']], ['date_order', '>=',this.state.date]]);
+        const data = await this.orm.searchCount("sale.order", [['state', 'in', ['sale', 'done']], ['date_order', '>=', this.state.date]]);
         this.state.ventas.value = data;
     }
 }
@@ -39,7 +38,5 @@ export class Dashboard extends Component {
 Dashboard.template = "TobaccoMetricsPro.Dashboard";
 Dashboard.components = { MetricCard, ChartRenderer };
 
-// Log to console when adding to the registry to confirm it's being executed   
 console.log("Adding Dashboard to the registry");
-
 registry.category("actions").add("TobaccoMetricsPro.Dashboard", Dashboard);
