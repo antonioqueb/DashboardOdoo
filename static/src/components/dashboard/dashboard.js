252186e8675cb28e186795d71325516d3e34573b
antonioqueb <cardstillo@gmail.com>
2023-10-14 21:27:04 -0600

    Up

--- static/src/components/dashboard/dashboard.js
+++ static/src/components/dashboard/dashboard.js
@@ -1,45 +1,160 @@
+
+
 /** @odoo-module */
 
-import { registry } from "@web/core/registry";
-import { MetricCard } from "../metric_card/metric_card.js";
-import { ChartRenderer } from "../chart_renderer/chart_renderer.js";
-import { useService } from "@web/core/utils/hooks"; 
-import { Component, useState } from "@odoo/owl"; // Importar useState desde @odoo/owl
+import { registry } from "@web/core/registry"
+import { MetricCard } from "./metric_card/metric_card"
+import { ChartRenderer } from "./chart_renderer/chart_renderer"
+import { useService } from "@web/core/utils/hooks"
+const { Component, onWillStart, useRef, onMounted, useState } = owl
 
-export class Dashboard extends Component {
-    constructor() {
-        super(...arguments);
-        this.state = useState({ 
-            ventas: {
-                title: "Ventas",
-                value: 0,
-                porcentage: 4,
+export class OwlSalesDashboard extends Component {
+    setup(){
+        this.state = useState({
+            quotations: {
+                value:10,
+                percentage:6,
             },
-            period: 90,
-        });
-        this.orm = useService("orm");
+            period:90,
+        })
+        this.orm = useService("orm")
+        this.actionService = useService("action")
+
+        onWillStart(async ()=>{
+            this.getDates()
+            await this.getQuotations()
+            await this.getOrders()
+        })
     }
 
-    willStart() {
-        return this.getQuotations(); 
-    }
-    
-    async onChangePeriod(ev) {
-        this.state.date = moment().subtract(this.state.period, "days").format("L");
-        await this.getQuotations();
+    async onChangePeriod(){
+        this.getDates()
+        await this.getQuotations()
+        await this.getOrders()
     }
 
-    async getQuotations() {
-        const data = await this.orm.searchCount("sale.order", [
-            ['state', 'in', ['sale', 'done']],
-            ['date_order', '>=', this.state.date]
-        ]);
-        this.state.ventas.value = data;
+    getDates(){
+        this.state.current_date = moment().subtract(this.state.period, 'days').format('L')
+        this.state.previous_date = moment().subtract(this.state.period * 2, 'days').format('L')
+    }
+
+    async getQuotations(){
+        let domain = [['state', 'in', ['sent', 'draft']]]
+        if (this.state.period > 0){
+            domain.push(['date_order','>', this.state.current_date])
+        }
+        const data = await this.orm.searchCount("sale.order", domain)
+        this.state.quotations.value = data
+
+        // previous period
+        let prev_domain = [['state', 'in', ['sent', 'draft']]]
+        if (this.state.period > 0){
+            prev_domain.push(['date_order','>', this.state.previous_date], ['date_order','<=', this.state.current_date])
+        }
+        const prev_data = await this.orm.searchCount("sale.order", prev_domain)
+        const percentage = ((data - prev_data)/prev_data) * 100
+        this.state.quotations.percentage = percentage.toFixed(2)
+    }
+
+    async getOrders(){
+        let domain = [['state', 'in', ['sale', 'done']]]
+        if (this.state.period > 0){
+            domain.push(['date_order','>', this.state.current_date])
+        }
+        const data = await this.orm.searchCount("sale.order", domain)
+        //this.state.quotations.value = data
+
+        // previous period
+        let prev_domain = [['state', 'in', ['sale', 'done']]]
+        if (this.state.period > 0){
+            prev_domain.push(['date_order','>', this.state.previous_date], ['date_order','<=', this.state.current_date])
+        }
+        const prev_data = await this.orm.searchCount("sale.order", prev_domain)
+        const percentage = ((data - prev_data)/prev_data) * 100
+        //this.state.quotations.percentage = percentage.toFixed(2)
+
+        //revenues
+        const current_revenue = await this.orm.readGroup("sale.order", domain, ["amount_total:sum"], [])
+        const prev_revenue = await this.orm.readGroup("sale.order", prev_domain, ["amount_total:sum"], [])
+        const revenue_percentage = ((current_revenue[0].amount_total - prev_revenue[0].amount_total) / prev_revenue[0].amount_total) * 100
+
+        //average
+        const current_average = await this.orm.readGroup("sale.order", domain, ["amount_total:avg"], [])
+        const prev_average = await this.orm.readGroup("sale.order", prev_domain, ["amount_total:avg"], [])
+        const average_percentage = ((current_average[0].amount_total - prev_average[0].amount_total) / prev_average[0].amount_total) * 100
+
+        this.state.orders = {
+            value: data,
+            percentage: percentage.toFixed(2),
+            revenue: `$${(current_revenue[0].amount_total/1000).toFixed(2)}K`,
+            revenue_percentage: revenue_percentage.toFixed(2),
+            average: `$${(current_average[0].amount_total/1000).toFixed(2)}K`,
+            average_percentage: average_percentage.toFixed(2),
+        }
+
+        //this.env.services.company
+    }
+
+    async viewQuotations(){
+        let domain = [['state', 'in', ['sent', 'draft']]]
+        if (this.state.period > 0){
+            domain.push(['date_order','>', this.state.current_date])
+        }
+
+        let list_view = await this.orm.searchRead("ir.model.data", [['name', '=', 'view_quotation_tree_with_onboarding']], ['res_id'])
+
+        this.actionService.doAction({
+            type: "ir.actions.act_window",
+            name: "Quotations",
+            res_model: "sale.order",
+            domain,
+            views: [
+                [list_view.length > 0 ? list_view[0].res_id : false, "list"],
+                [false, "form"],
+            ]
+        })
+    }
+
+    viewOrders(){
+        let domain = [['state', 'in', ['sale', 'done']]]
+        if (this.state.period > 0){
+            domain.push(['date_order','>', this.state.current_date])
+        }
+
+        this.actionService.doAction({
+            type: "ir.actions.act_window",
+            name: "Quotations",
+            res_model: "sale.order",
+            domain,
+            context: {group_by: ['date_order']},
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ]
+        })
+    }
+
+    viewRevenues(){
+        let domain = [['state', 'in', ['sale', 'done']]]
+        if (this.state.period > 0){
+            domain.push(['date_order','>', this.state.current_date])
+        }
+
+        this.actionService.doAction({
+            type: "ir.actions.act_window",
+            name: "Quotations",
+            res_model: "sale.order",
+            domain,
+            context: {group_by: ['date_order']},
+            views: [
+                [false, "pivot"],
+                [false, "form"],
+            ]
+        })
     }
 }
 
-Dashboard.template = "TobaccoMetricsPro.Dashboard";
-Dashboard.components = { MetricCard, ChartRenderer };
+TobaccoMetricsPro.template = "TobaccoMetricsPro.Dashboard"
+TobaccoMetricsPro.components = { MetricCard, ChartRenderer }
 
-console.log("Adding Dashboard to the registry");
-registry.category("actions").add("TobaccoMetricsPro.Dashboard", Dashboard);
+registry.category("actions").add("owl.sales_dashboard", OwlSalesDashboard)
