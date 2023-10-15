594afcb5ccc7cea3ed47d35592cbb4255f319552
antonioqueb <cardstillo@gmail.com>
2023-10-14 19:56:26 -0600

    Up

--- static/src/components/dashboard/dashboard.js
+++ static/src/components/dashboard/dashboard.js
@@ -3,7 +3,6 @@
 import { registry } from "@web/core/registry";
 import { MetricCard } from "../metric_card/metric_card.js";
 import { ChartRenderer } from "../chart_renderer/chart_renderer.js";
-import { loadJS } from "@web/core/assets";
 
 const { Component, tags, onWillStart, useRef, onMounted } = owl;
 
