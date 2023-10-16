7c62e40534a9146448e1b201fdbada6e5146d3a5
antonioqueb <cardstillo@gmail.com>
2023-10-15 23:23:05 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'TobaccoMetricsPro',
-    'version': '1.0.3', 
+    'version': '1.0.4', 
     'category': 'Administration',
     'summary': 'Herramientas de análisis y visualización para la industria tabacalera.',
     'author': 'Antonio Queb',
