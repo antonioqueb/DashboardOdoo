119f1e29ecc4dcf871674b64281dd5be4bd25927
antonioqueb <cardstillo@gmail.com>
2023-10-16 15:09:22 -0600

    Desarrollo de la semana #42 de producción de TobaccoMetricsPro - October ÑO 15:09:22

---
+++ commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+while true; do
+    # Agregar todos los cambios
+    git add .
+
+    # Obtener la fecha en el formato solicitado: semana del mes, mes, año, hora exacta
+    SEMANA=$(date '+%U')
+    MES=$(date '+%B')
+    AÑO=$(date '+%Y')
+    HORA=$(date '+%H:%M:%S')
+
+    # Construir el mensaje de commit
+    MSG_COMMIT="Desarrollo de la semana #$SEMANA de producción de TobaccoMetricsPro - $MES $AÑO $HORA"
+
+    # Realizar el commit
+    git commit -m "$MSG_COMMIT"
+
+    # Esperar 45 segundos antes de la próxima iteración
+    sleep 45
+done
