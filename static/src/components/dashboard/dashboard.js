dd2154c3a3ae8a82f36a45ea3ac552dce775ae11
antonioqueb <cardstillo@gmail.com>
2023-10-16 03:11:15 -0600

    Add getQuotations - Test

--- static/src/components/dashboard/dashboard.js
+++ static/src/components/dashboard/dashboard.js
@@ -28,6 +28,24 @@ export class Dashboard extends Component {
     await this.getOrders()
 
   }
+
+  async getQuotations(){
+    let domain = [['state', 'in', ['sent', 'draft']]]
+    if (this.state.period > 0){
+        domain.push(['date_order','>', this.state.current_date])
+    }
+    const data = await this.orm.searchCount("sale.order", domain)
+    this.state.quotations.value = data
+
+    // previous period
+    let prev_domain = [['state', 'in', ['sent', 'draft']]]
+    if (this.state.period > 0){
+        prev_domain.push(['date_order','>', this.state.previous_date], ['date_order','<=', this.state.current_date])
+    }
+    const prev_data = await this.orm.searchCount("sale.order", prev_domain)
+    const percentage = ((data - prev_data)/prev_data) * 100
+    this.state.quotations.percentage = percentage.toFixed(2)
+}
     
 }
 
