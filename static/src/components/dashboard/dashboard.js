7e3e05694d54e0511998986665aad0e1d653f21e
antonioqueb <cardstillo@gmail.com>
2023-10-16 13:33:40 -0600

    Test fix

--- static/src/components/dashboard/dashboard.js
+++ static/src/components/dashboard/dashboard.js
@@ -3,7 +3,7 @@
 import { registry } from "@web/core/registry";
 import { MetricCard } from "../metric_card/metric_card.js";
 import { ChartRenderer } from "../chart_renderer/chart_renderer.js";
-import { useService } from "@web/core/utils/hooks"
+import { useService } from "@web/core/utils/hooks";
 const { Component, onWillStart, useRef, onMounted, useState } = owl
 
 
@@ -11,8 +11,13 @@ export class Dashboard extends Component {
   setup(){
     
     this.state = useState({
-        quotations: {},
+        quotations: {
+            value: 0,
+            percentage: 0,
+        },
         period: 90,
+        current_date: null,
+        previous_date: null,
 
     })
     this.orm = useService("orm")
@@ -28,10 +33,12 @@ export class Dashboard extends Component {
 
   }
 
-  getDates(){
-    this.state.current_date = moment().subtract(this.state.period, 'days').format('L')
-    this.state.previous_date = moment().subtract(this.state.period * 2, 'days').format('L')
-  }
+    getDates() {
+        this.state.patch({
+            current_date: moment().subtract(this.state.period, 'days').format('L'),
+            previous_date: moment().subtract(this.state.period * 2, 'days').format('L')
+        });
+    }
 
   async getQuotations(){
     let domain = [['state', 'in', ['sent', 'draft']]]
@@ -41,7 +48,7 @@ export class Dashboard extends Component {
     const data = await this.orm.searchCount("sale.order", domain)
     this.state.quotations.value = data
 
-    // previous period yes of course
+    //    previous period yes of course
     let prev_domain = [['state', 'in', ['sent', 'draft']]]
     if (this.state.period > 0){
         prev_domain.push(['date_order','>', this.state.previous_date], ['date_order','<=', this.state.current_date])
@@ -134,7 +141,7 @@ async getSalesIncome(){
     }
 
     //this.env.services.company
-    }
+ }
 
 
 
