dca782aede2482d7489e2b2c26d777643101a608
antonioqueb <cardstillo@gmail.com>
2023-10-17 09:56:24 -0600

    Desarrollo de la semana #42 de producción de TobaccoMetricsPro -  09:56:24

--- commit.sh
+++ commit.sh
@@ -7,7 +7,7 @@ while true; do
     # Obtener la fecha en el formato solicitado: semana del mes, mes, año, hora exacta
     SEMANA=$(date '+%U')
     MES=$(date '+%B')
-    AÑO=$(date '+%Y')
+    ANO=$(date '+%Y')
     HORA=$(date '+%H:%M:%S')
 
     # Construir el mensaje de commit con la fecha
@@ -17,7 +17,7 @@ while true; do
     git commit -m "$MSG_COMMIT"
 
     # Subir los cambios al repositorio remoto en la rama "main"
-    git push origin main  # Asegúrate de ajustar "origin" y "main" según tu configuración
+    git push
 
     # Esperar 60 segundos antes de la próxima iteración parta evitar sobrecargar el servidor
     sleep 60
