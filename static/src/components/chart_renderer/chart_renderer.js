4b79799aaea479e7120c8356b4efac79eb6f5a99
antonioqueb <cardstillo@gmail.com>
2023-10-17 14:20:59 -0600

    Desarrollo de la semana #42 de producción de TobaccoMetricsPro -  14:20:59

--- static/src/components/chart_renderer/chart_renderer.js
+++ static/src/components/chart_renderer/chart_renderer.js
@@ -1,12 +1,12 @@
 /** @odoo-module */
 
-import { registry } from "@web/core/registry"
-import { loadJS } from "@web/core/assets"
-const { Component, onWillStart, useRef, onMounted } = owl
+import { registry } from "@web/core/registry";
+import { loadJS } from "@web/core/assets";
+const { Component, onWillStart, useRef, onMounted } = owl;
 
 export class ChartRenderer extends Component {
     setup() {
-        this.chartRef = useRef("chart")
+        this.chartRef = useRef("chart");
         this.staticData = {
           'Meses': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
             'Ventas netas': [120, 125, 130, 135, 139, 135, 139, 140, 145, 150, 155, 160],
@@ -16,12 +16,14 @@ export class ChartRenderer extends Component {
             'Total, Gasto de Venta': [20, 21, 22, 23, 25, 23, 30, 31, 32, 33, 34, 35],
             'Total, en Gastos': [26, 28, 30, 32, 32, 37, 36, 41, 43, 45, 47, 49],
             'Impuestos Federales': [7, 7, 8, 8, 8, 7, 7, 9, 9, 10, 10, 11],
-            'Utilidad Neta': [37, 39, 40, 42, 47, 37, 44, 35, 37, 38, 40, 41], 
-        onWillStart(async () => {
-            await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js")
-        })
+            'Utilidad Neta': [37, 39, 40, 42, 47, 37, 44, 35, 37, 38, 40, 41],
+         };
 
-        onMounted(() => this.renderChart())
+        onWillStart(async () => {
+            await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js");
+        });
+
+        onMounted(() => this.renderChart());
     }
     renderChart() {
         const ctx = this.chartRef.el.getContext('2d');
