0d4172bc235ba845a88c2eed01e8c7d7c4c63d58
antonioqueb <cardstillo@gmail.com>
2023-10-17 14:47:35 -0600

    Desarrollo de la semana #42 de producción de TobaccoMetricsPro -  14:47:35

--- static/src/components/metric_card/metric_card.js
+++ static/src/components/metric_card/metric_card.js
@@ -1,50 +1,16 @@
 /** @odoo-module */
 
-import { Component, onWillStart, useRef, onMounted } from 'owl';
-import { loadJS } from "@web/core/assets";
+const { Component } = owl;
 
 export class MetricCard extends Component {
-    constructor() {
-        super(...arguments);
-        this.chartRef = useRef("chart");
-        onWillStart(this.loadChartJS);
-        onMounted(this.renderChart);
-    }
-
-    async loadChartJS() {
-        await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.5.1/chart.min.js");
-        await loadJS("https://cdn.jsdelivr.net/npm/chartjs-plugin-gauge");
-    }
-
-    renderChart() {
-        const ctx = this.chartRef.el.getContext('2d');
-        new Chart(ctx, {
-            type: 'gauge',
-            data: {
-                datasets: [{
-                    value: this.props.value,
-                    data: [{value: this.props.value}],  // actualizar según la necesidad
-                    backgroundColor: ['red', 'orange', 'yellow', 'green'],
-                    borderWidth: 0
-                }]
-            },
-            options: {
-                plugins: {
-                    gauge: {
-                        valueDomain: [0, 100],
-                        valueLineCap: 'round'
-                    }
-                }
-            }
-        });
-    }
-
     get formattedValue() {
         return this.formatNumber(this.props.value, this.props.name);
     }
 
     formatNumber(num, name) {
         if (!num) return '0';
+
+        // Convertir la entrada a un número si es una cadena.
         if (typeof num === 'string') num = parseFloat(num);
 
         let formattedNumber;
@@ -61,6 +27,8 @@ export class MetricCard extends Component {
                 formattedNumber = num.toLocaleString();
                 break;
         }
+
+
 
         return formattedNumber;
     }
