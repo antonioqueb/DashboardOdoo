c70b4c6858267de047fb86ae4e58f0089629ea87
antonioqueb <cardstillo@gmail.com>
2023-10-17 15:13:25 -0600

    Desarrollo de la semana #42 de producción de TobaccoMetricsPro -  15:13:25

--- static/src/components/chart_renderer/chart_renderer.js
+++ static/src/components/chart_renderer/chart_renderer.js
@@ -70,20 +70,20 @@ export class ChartRenderer extends Component {
                         data: this.staticData['Ventas netas'],
                         fill: true,
                         borderColor: '#183b9c',
-                        backgroundColor: ['gradient11', 'gradient12', 'gradient13', 'gradient14', 'gradient15',
-                                          'gradient16', 'gradient17', 'gradient18', 'gradient19', 'gradient20',
-                                          'gradient21', 'gradient22', 'gradient23', 'gradient24', 'gradient25',
-                                          'gradient26', 'gradient27', 'gradient28', 'gradient29', 'gradient30']
+                        backgroundColor: [gradient11, gradient12, gradient13, gradient14, gradient15,
+                                          gradient16, gradient17, gradient18, gradient19, gradient20,
+                                          gradient21, gradient22, gradient23, gradient24, gradient25,
+                                          gradient26, gradient27, gradient28, gradient29, gradient30]
                         tension: 0.1
                     },
                     {
                         label: 'Resumen de Resultados',
                         data: this.staticData['Costo neto'],
                         fill: true,
-                        backgroundColor: ['gradient11', 'gradient12', 'gradient13', 'gradient14', 'gradient15',
-                                          'gradient16', 'gradient17', 'gradient18', 'gradient19', 'gradient20',
-                                          'gradient21', 'gradient22', 'gradient23', 'gradient24', 'gradient25',
-                                          'gradient26', 'gradient27', 'gradient28', 'gradient29', 'gradient30']
+                        backgroundColor: [gradient11, gradient12, gradient13, gradient14, gradient15,
+                                          gradient16, gradient17, gradient18, gradient19, gradient20,
+                                          gradient21, gradient22, gradient23, gradient24, gradient25,
+                                          gradient26, gradient27, gradient28, gradient29, gradient30]
                         borderColor: '#297eea',
                         tension: 0.1
                     }]
