0975f73123e22bc31dcc9c609aa5ac4286a459e6
antonioqueb <cardstillo@gmail.com>
2023-10-17 14:10:45 -0600

    Desarrollo de la semana #42 de producción de TobaccoMetricsPro -  14:10:45

--- static/src/components/chart_renderer/chart_renderer.js
+++ static/src/components/chart_renderer/chart_renderer.js
@@ -50,7 +50,7 @@ export class ChartRenderer extends Component {
                         label: 'Resumen de Resultados',
                         data: this.staticData['Ventas netas'],
                         fill: true,
-                        backgroundColor: '#0460D9',
+                        backgroundColor: '#027313',
                         borderColor: '#0439D9',
                         tension: 0.1
                     },
@@ -58,7 +58,7 @@ export class ChartRenderer extends Component {
                         label: 'Resumen de Resultados',
                         data: this.staticData['Costo neto'],
                         fill: true,
-                        backgroundColor: '#027313',
+                        backgroundColor: '#0460D9',
                         borderColor: '#0460D9',
                         tension: 0.1
                     }]
