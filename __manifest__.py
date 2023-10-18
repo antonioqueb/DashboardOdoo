4f39a8cc43776f297abd7aa5823c506b6e6303f8
antonioqueb <cardstillo@gmail.com>
2023-10-18 01:52:06 -0600

    Desarrollo de la semana #42 de producción de TobaccoMetricsPro -  01:52:06

--- __manifest__.py
+++ __manifest__.py
@@ -5,12 +5,12 @@
     'summary': 'Herramientas de análisis y visualización para la industria tabacalera.',
     'author': 'Antonio Queb',
     'website': 'https://www.fingrow.co',
-    'depends': ['base', 'web', 'board', 'sale','purchase'],
+    'depends': ['base', 'web', 'board', 'sale', 'purchase'],
     'assets': {
-    'web.assets_backend': [
-        'TobaccoMetricsPro/static/src/components/**/**.js',
-        'TobaccoMetricsPro/static/src/components/**/**.xml',
-    ]
+        'web.assets_backend': [
+            'TobaccoMetricsPro/static/src/components/**/**.js',
+            'TobaccoMetricsPro/static/src/components/**/**.xml',
+            ]
     },
     'data': [
         'views/menu.xml'
